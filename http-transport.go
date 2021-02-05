2abb3f899a35d631a91520d3027b9eb596879f2d
yangsen <yang_sen_ysys@163.com>
2021-02-05 18:49:17 +0800

    a

--- http-transport.go
+++ http-transport.go
@@ -24,7 +24,7 @@ func main() {
 	client.DefaultClient.Client = &http.Client{Transport: transport}
 
 
-	upload(1, 50)
+	upload(1000, 50)
 
 	fmt.Println("======= Done =======")
 }
@@ -64,6 +64,7 @@ func uploadFileToQiniu(index int) {
 
 	filePath := "/Users/senyang/Desktop/QiNiu/pycharm.dmg"
 	filePath = "/Users/senyang/Desktop/QiNiu/UploadResource_49M.zip"
+	filePath = "/Users/senyang/Desktop/QiNiu/Image/image.png"
 
 	key := "http3_test1" + fmt.Sprintf("%d", rand.Int())
 	token := "HwFOxpYCQU6oXoZXFOTh1mq5ZZig6Yyocgk3BTZZ:6MoNfPe6Tj6LaZXwSmRoY5PqcCA=:eyJzY29wZSI6ImtvZG8tcGhvbmUtem9uZTAtc3BhY2UiLCJkZWFkbGluZSI6MTYxNzUwNzUxMiwgInJldHVybkJvZHkiOiJ7XCJjYWxsYmFja1VybFwiOlwiaHR0cDpcL1wvY2FsbGJhY2suZGV2LnFpbml1LmlvXCIsIFwiZm9vXCI6JCh4OmZvbyksIFwiYmFyXCI6JCh4OmJhciksIFwibWltZVR5cGVcIjokKG1pbWVUeXBlKSwgXCJoYXNoXCI6JChldGFnKSwgXCJrZXlcIjokKGtleSksIFwiZm5hbWVcIjokKGZuYW1lKX0ifQ=="
@@ -78,7 +79,7 @@ func uploadFileToQiniu(index int) {
 	uploader := storage.NewResumeUploader(config)
 	ctx := context.Background()
 
-	var response interface{}
-	err := uploader.PutFile(ctx, response, token, key, filePath, nil)
+	var response storage.PutRet
+	err := uploader.PutFile(ctx, &response, token, key, filePath, nil)
 	fmt.Printf("index:%d key:%s error:%v response:%v \n", index, key, err, response)
 }
