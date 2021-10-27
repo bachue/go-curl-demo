ba34a533dbc0b84ec4b46648b82dccbaa0f75124
Bachue Zhou <bachue.shu@gmail.com>
2021-10-27 13:53:33 +0000

    upload by all ways

--- http-transport.go
+++ http-transport.go
@@ -7,10 +7,10 @@ import (
 	"net/http"
 	"sync"
 	"time"
-//      "crypto/tls"
+	//      "crypto/tls"
 
-    	"github.com/YangSen-qn/go-curl/v2/curl"
-  	"github.com/qiniu/go-sdk/v7/client"
+	"github.com/YangSen-qn/go-curl/v2/curl"
+	"github.com/qiniu/go-sdk/v7/client"
 	"github.com/qiniu/go-sdk/v7/storage"
 )
 
@@ -19,16 +19,16 @@ func main() {
 
 	transport := &curl.Transport{
 		Transport:      &http.Transport{},
- 		ForceHTTP3:     true,
+		ForceHTTP3:     true,
 		HTTP3LogEnable: true,
 	}
-//      transport := http.DefaultTransport.(*http.Transport)
-//      transport.ForceAttemptHTTP2 = false
-//	transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	//      transport := http.DefaultTransport.(*http.Transport)
+	//      transport.ForceAttemptHTTP2 = false
+	//      transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	client.DefaultClient.Client = &http.Client{Transport: transport, Timeout: 180 * time.Second}
 
-	// upload(1, 1)
-	upload(100000, 16)
+	upload(1, 1)
+	//	upload(10000, 16)
 
 	fmt.Println("======= Done =======")
 }
@@ -60,9 +60,9 @@ func upload(uploadCount int, goroutineCount int) {
 				} else {
 					key := "http3_test_" + time.Now().Format("2006/01/02 15:04:05.999999")
 					fmt.Printf("goroutineIndex:%d, index:%d, key:%v\n", goroutineIndex, index, key)
-                                        beginTime := time.Now()
+					beginTime := time.Now()
 					response, err := uploadFileToQiniu(key)
-                                        elapsed := time.Since(beginTime)
+					elapsed := time.Since(beginTime)
 					if err != nil {
 						fmt.Printf("goroutineIndex:%d, index:%d, key:%v, response:%v, elapsed:%v, err:%v\n", goroutineIndex, index, key, response, elapsed, err)
 					} else {
@@ -79,21 +79,29 @@ func upload(uploadCount int, goroutineCount int) {
 func uploadFileToQiniu(key string) (response storage.PutRet, err error) {
 	filePath := "/tmp/1m"
 
-	token := "0tf5awMVxwf8WrEvrjtbiZrdRZRJU-91JgCqTOC8:6DVqykXJUW8YBrB1FA90NpT5ybc=:eyJkZWFkbGluZSI6MTY2MzA1MTUwOCwic2NvcGUiOiJ6aG91cm9uZy1odHRwMy10ZXN0In0="
+	// token := "0tf5awMVxwf8WrEvrjtbiZrdRZRJU-91JgCqTOC8:6DVqykXJUW8YBrB1FA90NpT5ybc=:eyJkZWFkbGluZSI6MTY2MzA1MTUwOCwic2NvcGUiOiJ6aG91cm9uZy1odHRwMy10ZXN0In0="
+	token := "0p6YmsTFa-MLL4cZhe2Yj8n7nXZ3N_wk0ZmuZMnu:9Ws_xYpJN-4DQLqyAgKI0iemEw0=:eyJkZWFkbGluZSI6MTY2Njg3MjY2Mywic2NvcGUiOiJ6MC1idWNrZXQifQ=="
 
 	config := &storage.Config{
 		Zone: &storage.Region{
-			SrcUpHosts: []string{"up.qiniu.com"},
+			SrcUpHosts: []string{"up-z0.qbox.me"},
 		},
 		Region:   nil,
 		UseHTTPS: true,
 	}
 	ctx := context.Background()
 
-	// uploader := storage.NewResumeUploader(config)
-	// uploader := storage.NewResumeUploaderV2(config)
-	uploader := storage.NewFormUploader(config);
+	if err = storage.NewResumeUploader(config).PutFile(ctx, &response, token, key, filePath, nil); err != nil {
+		return
+	}
 
-	err = uploader.PutFile(ctx, &response, token, key, filePath, nil)
+	if err = storage.NewResumeUploaderV2(config).PutFile(ctx, &response, token, key, filePath, nil); err != nil {
+		return
+	}
+
+	if err = storage.NewFormUploader(config).PutFile(ctx, &response, token, key, filePath, nil); err != nil {
+		return
+	}
+
 	return
 }
